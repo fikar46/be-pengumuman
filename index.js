b610968dc42d1c84ff44823a8ea296182d51c0e6
zulfikar <zulfikar@doku.com>
2026-04-17 22:46:09 +0700

    fix: add cors headers for process pengumuman api

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import mysql from "mysql2/promise";
 import Redis from "ioredis";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.json());
 app.use((err, req, res, next) => {
   if (err && err.type === "entity.parse.failed") {
